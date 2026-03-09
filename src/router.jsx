c19a319bdff124b293864bd6693c5e278fa9c67a
ineshajri87-sketch <ineshajri87@gmail.com>
2026-03-09 12:01:25 +0100

    upadates

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
-
+import Dashboard from "./pages/Dashboard";
+import FileSearchPage from "./pages/FileSearchPage";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Login /> },
-  {path: "/register", element: < Register/>},
-  { path: "/dashboard", element: <Dashboard /> },
-  
+  { path: "/",                    element: <Login /> },
+  { path: "/register",            element: <Register /> },
+  { path: "/dashboard",           element: <Dashboard /> },
+  { path: "/dashboard/:type",        element: <FileSearchPage /> },
 ]);
